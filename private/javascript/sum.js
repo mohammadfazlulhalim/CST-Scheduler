5a532e54de21ee2f26567055083643da4398307b
Banman <banman9021@saskpolytech.ca>
2023-10-05 14:18:44 -0600

    added example test - DaveB

---
+++ private/javascript/sum.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the sum of the given values
+ * @param {number} a - first number to sum
+ * @param {number} b - second number to sum
+ * @return {*} - the sum of a and b
+ */
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
