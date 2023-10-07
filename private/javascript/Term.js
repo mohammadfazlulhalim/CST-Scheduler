36d65137abe54f4ac83ddccf61fc8b95072fc44d
Brandon <lysholm9452@saskpolytech.ca>
2023-10-07 16:17:08 -0600

    Added the first few files in proper structure and started stubbing out story32 - Brandon

---
+++ private/javascript/Term.js
@@ -0,0 +1,26 @@
+/**
+ * Need to add proper comment
+ */
+class Term {
+  /**
+     * Need to add proper comments
+     * @param startDate
+     * @param endDate
+     * @param termNumber
+     */
+  constructor(startDate, endDate, termNumber) {
+
+  }
+
+
+  /**
+     * Need to add comments
+     * @return {string}
+     */
+  retreiveList() {
+    return 'Not implemented yet';
+  }
+}
+
+// Documentation for using module exports: https://nodejs.org/api/modules.html#modules_module_exports
+// module.exports;
