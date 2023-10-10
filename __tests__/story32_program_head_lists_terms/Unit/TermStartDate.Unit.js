0ac776fa81096b929c1efefd4b58e7a06ff12ddf
Lysholm <lysholm9452@saskpolytech.ca>
2023-10-10 14:51:08 -0600

    Worked on my ORM to get it in proper syntax, started on a start date accept test and researched a reject date test - Brandon

--- __tests__/story32_program_head_lists_terms/Unit/TermStartDate.Unit.js
+++ __tests__/story32_program_head_lists_terms/Unit/TermStartDate.Unit.js
@@ -1,13 +1,14 @@
+// Importing the ORM object
 const Term = require('../../../private/javascript/Term.js');
 
-// Here is a function to remind myself of syntac
-// Get refresher at: https://jestjs.io/docs/getting-started
-const functionCall = 3;
-const expectedResult =3;
 
+// Here is a function to remind myself of syntax
 // This is working and passing, testing library is all working now
 test('Test title', () => {
-  expect(functionCall).toBe(expectedResult);
+    // Get refresher at: https://jestjs.io/docs/getting-started
+    const functionCall = 3;
+    const expectedResult = 3;
+    expect(functionCall).toBe(expectedResult);
 });
 
 // Now I just need to add in all my tests, and stubs for all my methods
@@ -15,70 +16,69 @@ test('Test title', () => {
 // As a group, how do we plan on doing things regarding classes and constructor restrictions
 
 // Term 1 Tests
-test('testThatTerm1AcceptsStartDateInAugust', ()=>{
-  // How do I call the term class? Need to figure out exports better, more research needed
+test('testThatTerm1AcceptsStartDateInAugust', () => {
+    // For an accept, it will be easy
+    //  1. Call constructor with arguments
+    const term1 = Term.create({startDate: 'start', endDate: 'end', termNumber: 1});
+    //  2. Check that object has valid info
+    expect(term1.startDate).toBe("August 1");
+});
 
-  // For an accept, it will be easy
-  //  1. Call constructor with arguments
-  //  2. Check that object has valid info
+test('testThatTerm1AcceptsStartDateInSeptember', () => {
 
 });
 
-test('testThatTerm1AcceptsStartDateInSeptember', ()=>{
-
-});
-
-test('testThatTerm1RejectsStartDateInJuly', ()=>{
-  // For a reject, we need to figure out what should be displayed, and catch error?
+test('testThatTerm1RejectsStartDateInJuly', () => {
+    // For a reject, we need to catch the error message
 
 });
 
 // Term 4 Tests
-test('testThatTerm4AcceptsStartDateInAugust', ()=>{
+test('testThatTerm4AcceptsStartDateInAugust', () => {
 
 });
 
-test('testThatTerm4AcceptsStartDateInSeptember', ()=>{
+test('testThatTerm4AcceptsStartDateInSeptember', () => {
 
 });
 
-test('testThatTerm4RejectsStartDateInJuly', ()=>{
+test('testThatTerm4RejectsStartDateInJuly', () => {
 
 });
 
 // Term 2 Tests
-test('testThatTerm2AcceptsStartDateInJanuary', ()=>{
+test('testThatTerm2AcceptsStartDateInJanuary', () => {
 
 });
 
-test('testThatTerm2RejectsStartDateInDecember', ()=>{
+test('testThatTerm2RejectsStartDateInDecember', () => {
 
 });
 
 // Term 5 Tests
-test('testThatTerm5AcceptsStartDateInJanuary', ()=>{
+test('testThatTerm5AcceptsStartDateInJanuary', () => {
 
 });
 
-test('testThatTerm5RejectsStartDateInDecember', ()=>{
+test('testThatTerm5RejectsStartDateInDecember', () => {
 
 });
 
 // Term 3
-test('testThatTerm3AcceptsStartDateInMay', ()=>{
+test('testThatTerm3AcceptsStartDateInMay', () => {
 
 });
 
-test('testThatTerm3RejectsStartDateInApril', ()=>{
+test('testThatTerm3RejectsStartDateInApril', () => {
 
 });
 
 // Term 6
-test('testThatTerm6AcceptsStartDateInMay', ()=>{
+test('testThatTerm6AcceptsStartDateInMay', () => {
 
 });
 
-test('testThatTerm6RejectsStartDateInApril', ()=>{
+test('testThatTerm6RejectsStartDateInApril', () => {
 
 });
 
--- private/javascript/Term.js
+++ private/javascript/Term.js
@@ -1,26 +1,40 @@
-/**
- * Need to add proper comment
- */
-class Term {
-  /**
-     * Need to add proper comments
-     * @param startDate
-     * @param endDate
-     * @param termNumber
-     */
-  constructor(startDate, endDate, termNumber) {
+// Documentation for sequelize: https://sequelize.org/
 
+const {Sequelize, DataTypes} = require ('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '/private/database',
+  // instead of using persistent storage, going to try using the in memory syntax provided on sequelize docs
+  // 'sqlite::memory'
+});
+
+// Creating the model
+// See: https://sequelize.org/docs/v6/core-concepts/model-basics/
+const Term = sequelize.define('Term', {
+  startDate: {
+    type: DataTypes.DATEONLY,
+    //Here is where I can put my validators
+    // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+  },
+  endDate: {
+    type: DataTypes.DATEONLY,
+    //add validators
+    // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+  },
+  termNumber: {
+    type: DataTypes.INTEGER,
+    // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
   }
+});
 
+// Now need to keep it in sync with the database
+sequelize.sync()
+    .then(() => {
+      console.log('Term table created');
+    })
+    .catch((err) => {
+      console.error('Error creating Term table:', err);
+    });
 
-  /**
-     * Need to add comments
-     * @return {string}
-     */
-  retreiveList() {
-    return 'Not implemented yet';
-  }
-}
-
-// Documentation for using module exports: https://nodejs.org/api/modules.html#modules_module_exports
-// module.exports;
+module.exports = Term;
