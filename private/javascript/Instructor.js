849e631afe05ad97ebfc52c117713d03fd8d090d
Mason <rayburnharman7728@saskpolytech.ca>
2023-10-11 22:29:35 -0600

    Created branch, set up database objects and files

---
+++ private/javascript/Instructor.js
@@ -0,0 +1,12 @@
+let firstName;
+let lastName;
+
+/**
+ * constructor for Instructor
+ * @param first First name of instructor
+ * @param last Last name of instructor
+ * @constructor
+ */
+function Instructor(first, last) {
+}
+
