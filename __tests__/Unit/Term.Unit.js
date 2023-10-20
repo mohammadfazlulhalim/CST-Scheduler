0714da44a336cdf486ba56112d5310684a1afdf9
Brandon Lysholm <lysholm9452@saskpolytech.ca>
2023-10-19 21:28:52 -0600

    Added validation and tests for all of term 1 and term 4 start and end dates - Brandon

--- __tests__/Unit/Term.Unit.js
+++ __tests__/Unit/Term.Unit.js
@@ -1,71 +1,65 @@
 // Importing the ORM object
 const Term = require('../../private/javascript/Term.js');
 const {sequelize}= require('../../datasource');
-//const {Sequelize} = require('sequelize');
+// const {DATE, DATEONLY} = require('sequelize');
+// const {Sequelize} = require('sequelize');
 
 // Term.sequelize = new Sequelize('sqlite:memory');
 
 describe('startDate', () => {
-    let term1;
+  let term1;
 
-    beforeAll(async () => {
-        try {
-            await sequelize.sync();
-            //console.log('Terms table created successfully');
+  beforeAll(async () => {
+    try {
+      await sequelize.sync();
+      // console.log('Terms table created successfully');
+    } catch (error) {
+      console.error('Error creating Term table: ', error);
+    }
+  });
 
-        } catch (error) {
-            console.error('Error creating Term table: ', error);
-        }
-    });
+  beforeEach(async () => {
+    term1 = await Term.create({termNumber: 1, startDate: '2023-08-01', endDate: '2023-12-01'});
+  });
 
-    beforeEach(async () => {
-        term1 = await new Term({startDate: '2023-08-01', endDate: '2023-12-01', termNumber: 1})
-    });
+  // Term 1 Tests
+  test('testThatTerm1StartDateInAugustPass', async () => {
+    // For a pass, it will be easy
+    //  1. Call constructor with arguments
+    term1.startDate='2023-08-01';
+    await Term.create(term1);
+    //  2. Check that object is created and has valid info
+    expect(term1).toBeTruthy();
+    expect(term1.startDate).toBe('2023-08-01');
 
+    // Check for no errors
+  });
 
-// Here is a function to remind myself of syntax
-// This is working and passing, testing library is all working now
-    test('Test title ExampleSyntax', () => {
-        // Get refresher at: https://jestjs.io/docs/getting-started
-        const functionCall = 3;
-        const expectedResult = 3;
-        expect(functionCall).toBe(expectedResult);
-    });
+  test('testThatTerm1StartDateInSeptemberPass', async () => {
+    term1.startDate='2023-09-01';
+    await Term.create(term1);
+    expect(term1).toBeTruthy();
+    expect(term1.startDate).toBe('2023-09-01');
+  });
 
-// // Term 1 Tests
-//     test('testThatTerm1StartDateInAugustPass', async () => {
-//         // For a pass, it will be easy
-//         //  1. Call constructor with arguments
-//         //term1 = await new Term({startDate: '2023-08-01', endDate: '2023-12-01', termNumber: 1});
-//         //  2. Check that object is created and has valid info
-//         //expect(term1).toBeTruthy();
-//         //expect(term1.startDate).toBe('2023-08-01');
-//
-//         // Check for no errors
-//         //expect(term1.validate()).resolves.toBe(undefined);
-//
-//     });
-//
-//     test('testThatTerm1StartDateInSeptemberPass', async () => {
-//         //term1 = await new Term({startDate: '2023-09-01', endDate: '2023-12-01', termNumber: 1});
-//         //expect(term1).toBeTruthy();
-//         //expect(term1.startDate).toBe('2023-09-01');
-//
-//         //expect(term1.validate()).resolves.toBe(undefined);
-//
-//
-//     });
-//
-//     test('testThatTerm1StartDateInJulyFail', async () => {
-//         // For a fail, we need to catch the error message
-//         try {
-//             //term1 = await new Term({startDate: '2023-07-01', endDate: '2023-12-01', termNumber: 1});
-//         } catch (err) {
-//             //expect(err.errors[0].message).toBe('Term 1 must start in August or September');
-//         }
-//
-//     });
-//
+  test('testThatTerm1StartDateInJulyFail', async () => {
+    // For a fail, we need to catch the error message
+    let bCaughtErr = false;
+    try {
+      await Term.create({termNumber: 1, startDate: '2023-07-01', endDate: '2023-12-01'});
+      console.log('no thrown error');
+    } catch (err) {
+      console.log('thrown error: ' + err);
+      bCaughtErr = true;
+      expect(err.errors.length).toBe(1);
+      expect(err.errors[0].message).toBe('Term 1 must start in August or September');
+    }
+
+    if (!bCaughtErr) {
+      expect(1).toBe(2);
+    }
+  });
+
 // // Term 4 Tests
 //     test('testThatTerm4StartDateInAugustPass', () => {
 //
@@ -117,79 +111,188 @@ describe('startDate', () => {
 });
 
 describe('termNumber', () => {
-    let term1;
+  let term1;
 
-    beforeAll(async () => {
-        try {
-            await sequelize.sync();
-            //console.log('Terms table created successfully');
+  beforeAll(async () => {
+    try {
+      await sequelize.sync();
+      // console.log('Terms table created successfully');
+    } catch (error) {
+      console.error('Error creating Term table: ', error);
+    }
+  });
 
-        } catch (error) {
-            console.error('Error creating Term table: ', error);
-        }
-    });
-
-    beforeEach(async () => {
-        term1 = await new Term({startDate: '2023-08-01', endDate: '2023-12-01', termNumber: 1})
-    });
-
-
-    test('testThatTermLowNumberPass', async () => {
-        // For an accept, it will be easy
-        //  1. Call constructor with arguments
-        term1.termNumber =1;
-        await Term.create({startDate: '2023-08-01', endDate: '2023-12-01', termNumber: 1});
-        //  2. Check that object is created and has valid info
-        expect(term1).toBeDefined();
-        expect(term1.termNumber).toBe(1);
-
-        // Check for no errors
-        //expect(term1.validate()).resolves.toBe(undefined);
-
-    });
-
-    test('testThatTermBelowOneFail', async () => {
-        // For a reject, we need to catch the error message
-        term1.termNumber =1;
-        let bCaughtErr = false;
-        try {
-            // Need to clean this up
-            await Term.create({startDate: '2023-08-01', endDate: '2023-12-01', termNumber: 0});
-        } catch (err) {
-            bCaughtErr = true;
-            expect(err.errors.length).toBe(1);
-            expect(err.errors[0].message).toBe('Term number must be between 1 and 6');
-        }
-
-        if (!bCaughtErr) {
-            expect(1).toBe(2);
-        }
-
-    });
-
-    test('testThatTermHighNumberPass', async () => {
-        term1.termNumber =6;
-        await Term.create(term1);
-        //  2. Check that object is created and has valid info
-        expect(term1).toBeDefined();
-        expect(term1.termNumber).toBe(6);
-
-    });
-
-    // test('testThatTermAboveSixFail', async () => {
-    //     term1.termNumber =7;
-    //     try {
-    //         await Term.create(term1);
-    //         fail('The validator did not throw errors');
-    //     } catch (err) {
-    //         expect(err.errors.length).toBe(1);
-    //         expect(err.errors[0].message).toBe('Term number must be between 1 and 6');
-    //
-    //         //expect(error.message).toBe('Term number must be between 1 and 6');
-    //     }
-    //
-    // });
+  beforeEach(async () => {
+    term1 = await new Term({termNumber: 1, startDate: '2023-09-01', endDate: '2023-12-01'});
+  });
 
 
+  test('testThatTermLowNumberPass', async () => {
+    // For an accept, it will be easy
+    //  1. Call constructor with arguments
+    term1.termNumber =1;
+    await Term.create(term1);
+    //  2. Check that object is created and has valid info
+    expect(term1).toBeDefined();
+    expect(term1.termNumber).toBe(1);
 
+    // Check for no errors
+    // expect(term1.validate()).resolves.toBe(undefined);
+  });
+
+  test('testThatTermBelowOneFail', async () => {
+    // For a reject, we need to catch the error message
+    term1.termNumber = 0;
+    let bCaughtErr = false;
+    try {
+      // If I just pass in term1, after changing termNumber, it fails weirdly
+      await Term.create({termNumber: term1.termNumber, startDate: '2023-09-01', endDate: '2023-12-01'});
+    } catch (err) {
+      bCaughtErr = true;
+      expect(err.errors.length).toBe(1);
+      expect(err.errors[0].message).toBe('Term number must be between 1 and 6');
+    }
+
+    if (!bCaughtErr) {
+      expect(1).toBe(2);
+    }
+  });
+
+  test('testThatTermHighNumberPass', async () => {
+    term1.termNumber =6;
+    await Term.create(term1);
+    //  2. Check that object is created and has valid info
+    expect(term1).toBeDefined();
+    expect(term1.termNumber).toBe(6);
+  });
+
+  test('testThatTermAboveSixFail', async () => {
+    term1.termNumber =7;
+    let bCaughtErr = false;
+    try {
+      await Term.create({termNumber: term1.termNumber, startDate: '2023-09-01', endDate: '2023-12-01'});
+    } catch (err) {
+      bCaughtErr = true;
+      // console.log('error 1 is : ' + err.errors[0].message);
+      // console.log('error 2 is : ' + err.errors[1].message);
+      expect(err.errors.length).toBe(1);
+      expect(err.errors[0].message).toBe('Term number must be between 1 and 6');
+    }
+
+    if (!bCaughtErr) {
+      expect(1).toBe(2);
+    }
+  });
+});
+
+describe('endDate', () => {
+  let term1;
+  let term4;
+
+  beforeAll(async () => {
+    try {
+      await sequelize.sync();
+      // console.log('Terms table created successfully');
+    } catch (error) {
+      console.error('Error creating Term table: ', error);
+    }
+  });
+
+  beforeEach(async () => {
+    term1 = await new Term({termNumber: 1, startDate: '2023-09-01', endDate: '2023-12-01'});
+    term4 = await new Term({termNumber: 4, startDate: '2023-09-01', endDate: '2023-12-01'});
+  });
+
+  // Term 1 Tests
+
+  test('testThatTerm1EndDateInDecemberPass', async ()=>{
+    term1.endDate= '2023-12-01';
+    await Term.create(term1);
+    expect(term1).toBeTruthy();
+    expect(term1.endDate).toBe('2023-12-01');
+  });
+
+  test('testThatTerm1EndDateInJanuaryFail', async ()=>{
+    let bCaughtErr=false;
+    try {
+      await Term.create({termNumber: 1, startDate: '2023-09-01', endDate: '2023-01-01'});
+    } catch (err) {
+      console.log('thrown error: ' + err);
+      bCaughtErr = true;
+      expect(err.errors.length).toBe(1);
+      expect(err.errors[0].message).toBe('Term 1 must end in December');
+    }
+
+    if (!bCaughtErr) {
+      expect(1).toBe(2);
+    }
+  });
+
+  // Term 4 Tests
+  test('testThatTerm4EndDateInDecemberPass', async ()=>{
+    term4.endDate= '2023-12-01';
+    await Term.create(term4);
+    expect(term1).toBeTruthy();
+    expect(term1.endDate).toBe('2023-12-01');
+  });
+
+  test('testThatTerm4EndDateInJanuaryFail', async ()=>{
+    let bCaughtErr=false;
+    try {
+      await Term.create({termNumber: 4, startDate: '2023-09-01', endDate: '2023-01-01'});
+    } catch (err) {
+      console.log('thrown error: ' + err);
+      bCaughtErr = true;
+      expect(err.errors.length).toBe(1);
+      expect(err.errors[0].message).toBe('Term 4 must end in December');
+    }
+
+    if (!bCaughtErr) {
+      expect(1).toBe(2);
+    }
+  });
+  //
+  // // Term 2 Tests
+  // test('testThatTerm2EndDateInAprilPass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm2EndDateInMayFail', ()=>{
+  //
+  // });
+  //
+  // // Term 5 Tests
+  // test('testThatTerm5EndDateInAprilPass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm5EndDateInMayFail', ()=>{
+  //
+  // });
+  //
+  // // Term 3 Tests
+  // test('testThatTerm3EndDateInMayPass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm3EndDateInJunePass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm3EndDateInJulyFail', ()=>{
+  //
+  // });
+  //
+  // // Term 6 Tests
+  // test('testThatTerm6EndDateInMayPass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm6EndDateInJunePass', ()=>{
+  //
+  // });
+  //
+  // test('testThatTerm6EndDateInJulyFail', ()=>{
+  //
+  // });
 });
--- private/javascript/Term.js
+++ private/javascript/Term.js
@@ -1,55 +1,87 @@
 // Documentation for sequelize: https://sequelize.org/
 
-//const {sequelize, DataTypes} = require('../../app');
+// const {sequelize, DataTypes} = require('../../app');
 const {DataTypes, sequelize} = require('../../datasource');
 
 // Creating the model
 // See: https://sequelize.org/docs/v6/core-concepts/model-basics/
 
 const Term = sequelize.define('Term', {
-    startDate: {
-        type: DataTypes.DATEONLY,
-        validate: {
-            //Here is where I can put my validators
-            // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
-            customValidator(value) {
-                // Going to do a check that it is not null
-                // if (value===null) {
-                //     throw new Error('Start date must be entered');
-                // }
-                // // This should probably be a switch statement later on
-                // if (termNumber===1)
-                // {
-                //     // getMonth() returns a zero-based value
-                //     if (value.getMonth() !==7 || value.Month !==8)
-                //     {
-                //         throw new Error('Term 1 must start in August or September');
-                //     }
-                // }
-            }
-        }
-    },
-    endDate: {
-        type: DataTypes.DATEONLY,
-        validate: {
-            //add validators
-            // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
-        }
-    },
     termNumber: {
-        type: DataTypes.INTEGER,
-        validate: {
-            min: {
-                args: 1,
-                msg: 'Term number must be between 1 and 6',
-            },
-            // max: {
-            //     args: 6,
-            //     msg: 'Term number must be between 1 and 6',
-            // }
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: 1,
+          msg: 'Term number must be between 1 and 6',
+        },
+        max: {
+          args: 6,
+          msg: 'Term number must be between 1 and 6',
+        },
+      },
+    },
+
+  startDate: {
+    type: DataTypes.DATEONLY,
+    validate: {
+      // Here is where I can put my validators
+      // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+      customTermStartDates(value) {
+          // splitting the date based on this
+          const dateArray = value.split('-');
+          const monthVal = dateArray[1];
+        if (this.termNumber===1) {
+          if (monthVal==='08' || monthVal==='09') {
+          } else {
+              throw new Error('Term 1 must start in August or September');
+          }
         }
-    }
-});
+        if (this.termNumber===4) {
+          if (monthVal==='08' || monthVal==='09') {
+          } else {
+            throw new Error('Term 4 must start in August or September');
+          }
+        }
+      },
+    },
+  },
+  endDate: {
+    type: DataTypes.DATEONLY,
+    validate: {
+      // Here is where I can put my validators
+      // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+      customEndDates(value) {
+        // splitting the date based on this
+        const dateArray = value.split('-');
+        const monthVal = dateArray[1];
+        if (this.termNumber===1) {
+          if (monthVal!=='12') {
+            throw new Error('Term 1 must end in December');
+          }
+        }
+        if (this.termNumber===4) {
+          if (monthVal!=='12') {
+            throw new Error('Term 4 must end in December');
+          }
+        }
+      },
+    },
+  },
+
+}, // , {
+//   validate: {
+//     startDatePerTerm() {
+//       // Here I can reference multiple fields
+//       if (termNumber === 1) {
+//         // Not sure if month is 0 based or not
+//         if (startDate.month!==7 || startDate.month!==8) {
+//           throw new Error('Term 1 must start in August or September');
+//         }
+//       }
+//     },
+//   },
+// }
+);
 
 // Now need to keep it in sync with the database
 
