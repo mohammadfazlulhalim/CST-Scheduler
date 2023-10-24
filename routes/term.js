cf6e8f2eb34b1c0060461868af25389ec89465c0
Lysholm <lysholm9452@saskpolytech.ca>
2023-10-24 15:08:50 -0600

    Changed collections variable name to match class diagram for term.js - Brandon

--- routes/term.js
+++ routes/term.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const Term = require('../private/javascript/Term');
 router.get('/', async function(req, res, next) {
   // Declaring the array
-  let termEntries;
+  let termLists;
 
   try {
     // Calling the database, for all term entries, ordered by term number
-    termEntries = await Term.findAll({order: ['termNumber']});
+    termLists = await Term.findAll({order: ['termNumber']});
   } catch (err) {
     // If it is not found, declaring termEntries as undefined so that table will not be viewed on term.hbs
     // and instead a sentence delaring no term entries found is displayed
-    termEntries = undefined;
+    termLists = undefined;
   }
   res.render('term', {
     title: 'Term Listings',
-    termEntries,
+    termEntries: termLists,
   });
 });
 
