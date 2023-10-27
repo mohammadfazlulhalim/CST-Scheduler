f0c844415d0012ccb1dfe868c00b68e097ec65d2
Jonah <dinh3453@saskpolytech.ca>
2023-10-27 12:12:38 -0600

    Ready for demo and merge

--- Fixtures/Classroom.fix.js
+++ Fixtures/Classroom.fix.js
@@ -1,22 +1,19 @@
 const Classroom = require('../private/javascript/Classroom');
 
-// eslint-disable-next-line require-jsdoc
-
-// eslint-disable-next-line require-jsdoc
+/**
+ * This clears the table for Classroom and then recreates the table
+ * with valid entries
+ * @return {Promise<void>}
+ */
 async function fillClassroomTable() {
   await Classroom.sync({force: true});
-  createClassrooms();
+  await createClassrooms();
 }
 
 // eslint-disable-next-line require-jsdoc
 async function createClassrooms() {
   const createdClassrooms = [
-    '239A', '260', '124', '301', '108B',
-    '215', '312', '203A', '128', '309',
-    '222', '110', '304A', '132', '211',
-    '307', '204', '126A', '230', '313',
-    'C2', 'R7', 'Lab3', 'X9Y', 'A-B',
-    'Room10', 'AB12', 'Q$', '7-8', 'Z#',
+    '239A', '239B', '240B', '242C',
   ];
   for (const roomNumber of createdClassrooms) {
     await Classroom.create({
@@ -25,4 +22,5 @@ async function createClassrooms() {
     });
   }
 }
+
 module.exports = fillClassroomTable;
--- fixtures/loadAll.fix.js
+++ fixtures/loadAll.fix.js
@@ -1,8 +1,10 @@
 // import individual fixture files here
 const fillCourseOfferingTable = require('./CourseOffering.fix');
 const fillTermTable = require('./Term.fix');
+const fillClassroomTable = require('./Classroom.fix');
 
 // call individual fixture methods here
 fillTermTable();
 fillCourseOfferingTable();
+fillClassroomTable();
 
