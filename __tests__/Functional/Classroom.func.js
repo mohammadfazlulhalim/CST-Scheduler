c7e59970557eedf0c3fef291842210d3c7bae265
Lysholm <lysholm9452@saskpolytech.ca>
2023-11-07 13:53:15 -0600

    Added Classroom.func and added the skeleton for functional tests on create statements - Brandon

---
+++ __tests__/Functional/Classroom.func.js
@@ -0,0 +1,37 @@
+const Classroom = require('../../private/javascript/Classroom');
+const {sequelize} = require('../../datasource');
+
+describe('create', ()=>{
+  let classroomInstance;
+
+  // Before all tests, create the Classroom table in the database
+  beforeAll(async () => {
+    try {
+      await sequelize.sync();
+      console.log('Classroom table created successfully');
+    } catch (error) {
+      console.error('Error creating Classroom table:', error);
+    }
+  });
+
+  // Valid classroom
+  beforeEach(async () => {
+    classroomInstance = await Classroom.create({roomNumber: '239A'});
+  });
+
+
+  // Test that a valid room has been added to the database
+  test('testThatValidRoomIsAddedToDatabase', async() => {
+    const pk = classroomInstance.primaryKey;
+    expect(Classroom.findByPk(pk)).toBeDefined();
+  });
+
+  test('testThatInValidRoomIsAddedToDatabase', async() =>{
+    try {
+      await Classroom.create({roomNumber: '12345678901'});
+    } catch (error) {
+      // Going to do nothing with the catch, but needed
+    }
+    expect(Classroom.findOne(roomNumber='12345678901')).toBeUndefined();
+  })
+})
