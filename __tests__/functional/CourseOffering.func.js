b76794b87debb8089b889585b34d4960b1dccdf4
Mason <rayburnharman7728@saskpolytech.ca>
2023-11-07 14:50:02 -0600

    created CourseOffering.func.js - Mason

---
+++ __tests__/functional/CourseOffering.func.js
@@ -0,0 +1,26 @@
+const CourseOffering = require('../../private/javascript/CourseOffering');
+const testConst = require('../../private/javascript/constants').testConst;
+
+escribe('group', () => {
+    let testUser;
+    // set up a valid user
+    beforeEach(async function() {
+        // drop the table and re-create it
+        await CourseOffering.sync({force: true});
+        testUser = testConst.courseOffering1;
+    });
+
+    test('testThatGroupWithOneLetterIsValid', async function() {
+        const groupLetter = 'A';
+        testUser.group = groupLetter;
+        const courseOffering = await CourseOffering.create(testUser);
+
+        expect(courseOffering).toBeTruthy();
+        expect(courseOffering.group).toBe(groupLetter);
+
+        // if valid, validate() returns the object it was validating
+        // if invalid, it returns errors
+        expect(await courseOffering.validate()).toBe(courseOffering);
+    });
+
+});
