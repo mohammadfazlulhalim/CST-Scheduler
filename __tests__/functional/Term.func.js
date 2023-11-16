0be8edc045211dfc7b2dcfd2e44e6ab2c900309b
Banman <banman9021@saskpolytech.ca>
2023-11-16 14:52:48 -0600

    Added more functional tests for Term - Dave

--- __tests__/functional/Term.func.js
+++ __tests__/functional/Term.func.js
@@ -16,7 +16,7 @@ describe('Terms in database', () => {
 
   test('testThatValidTermPostAddsToList', async () => {
     // create a valid term in the database
-    supertest(app).post('/term').send(testTerm).expect(201); // expect success: created
+    await supertest(app).post('/term').send(testTerm).expect(201); // expect success: created
     // find the newly added term to ensure it exists in the database
     const foundTerm = await Term.findOne({where: {
       termNumber: testTerm.termNumber,
@@ -32,22 +32,83 @@ describe('Terms in database', () => {
     // change term number so that the term object is invalid
     testTerm.termNumber = 3;
     // attempt to create the term in the database
-    supertest(app).post('/term').send(testTerm).expect('422'); // expect 422: unprocessable entity
+    await supertest(app).post('/term').send(testTerm).expect('422'); // expect 422: unprocessable entity
     // since no term should have been added to the database, the number of terms should remain the same
     const newNumTerms = (await Term.findAll()).length;
     expect(newNumTerms).toBe(oldNumTerms);
   });
 
-  test('testThatTermIsRemovedWhenTermIsDeleted', async () => {
+  test('testThatValidTermDeleteRemovesFromList', async () => {
     // store initial number of terms to compare against later
     const oldNumTerms = (await Term.findAll()).length;
     // create a new term to delete, then promptly delete it
     // testTerm does not have an ID, so use newTerm instead
     const newTerm = await Term.create(testTerm);
-    supertest(app).delete('/term').send(newTerm).expect(200); // expect 200: OK
+    // delete the term
+    await supertest(app).delete('/term').send(newTerm).expect(200); // expect 200: OK
 
     // If the term was deleted successfully, the number of terms in the database should
     // remain the same as it was before the 'create' statement
+    const newNumTerms = (await Term.findAll()).length;
+    expect(newNumTerms).toBe(oldNumTerms);
+  });
+
+  test('testThatInvalidTermDeleteDoesNotRemoveFromList', async () => {
+    const oldNumTerms = (await Term.findAll()).length;
+    testTerm.id = 1;
+    // try to delete a non-existent term
+    await supertest(app).delete('/term').send(testTerm).expect(404); // expect 404: not found
+    const newNumTerms = (await Term.findAll()).length;
+    expect(newNumTerms).toBe(oldNumTerms);
+  });
+
+  test('testThatValidTermPutUpdatesList', async () => {
+    // create a new term to update
+    const termToUpdate = Term.create(testTerm);
+    // store initial number of terms to compare against later
+    const oldNumTerms = (await Term.findAll()).length;
+    // update the newly added term
+    await supertest(app).put('/term').send({
+      id: termToUpdate.id,
+      startDate: testTerm.startDate,
+      endDate: '2023-12-15',
+      termNumber: termToUpdate.termNumber,
+    }).expect(200); // expect 200: OK
+    // ensure it didn't add a new term
+    const newNumTerms = (await Term.findAll()).length;
+    expect(newNumTerms).toBe(oldNumTerms);
+  });
+
+  test('testThatInvalidTermPutDoesNotUpdateList', async () => {
+    // create a new term to update
+    const termToUpdate = Term.create(testTerm);
+    // store initial number of terms to compare against later
+    const oldNumTerms = (await Term.findAll()).length;
+    // try to update the newly added term
+    await supertest(app).put('/term').send({
+      id: termToUpdate.id,
+      startDate: testTerm.startDate,
+      endDate: '2024-01-01',
+      termNumber: termToUpdate.termNumber,
+    }).expect(422); // expect 422: unprocessable entity
+    // ensure it didn't add a new term
+    const newNumTerms = (await Term.findAll()).length;
+    expect(newNumTerms).toBe(oldNumTerms);
+  });
+
+  test('testThatNonExistentTermCannotBeUpdated', async () => {
+    // create a new term to update
+    const termToUpdate = Term.create(testTerm);
+    // store initial number of terms to compare against later
+    const oldNumTerms = (await Term.findAll()).length;
+    // try to update the newly added term
+    await supertest(app).put('/term').send({
+      id: termToUpdate.id + 1, // non-existent ID
+      startDate: testTerm.startDate,
+      endDate: testTerm.startDate,
+      termNumber: 3,
+    }).expect(404); // expect 404: not found
+    // ensure it didn't add a new term
     const newNumTerms = (await Term.findAll()).length;
     expect(newNumTerms).toBe(oldNumTerms);
   });
