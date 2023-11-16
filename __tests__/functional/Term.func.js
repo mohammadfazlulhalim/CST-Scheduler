d41446d539187189aa96b83fa41725f1e0fc7353
Banman <banman9021@saskpolytech.ca>
2023-11-16 13:50:27 -0600

    Integrated supertest into the functional tests for Term - Dave

--- __tests__/functional/Term.func.js
+++ __tests__/functional/Term.func.js
@@ -1,40 +1,38 @@
 // Importing the ORM object
 const Term = require('../../private/javascript/Term.js');
-const {sequelize} = require('../../dataSource');
-const {createTerm, deleteTerm, updateTerm} = require('../../routes/term');
+const supertest = require('supertest');
+const app = require('../../app');
 
 // this set of tests ensures that Term objects are handled within the database properly
-describe('Terms in database', async () => {
+describe('Terms in database', () => {
   let testTerm;
 
   // create a valid term to use as a base
   beforeEach(async () => {
+    // drop the table and re-create it
+    await Term.sync({force: true});
     testTerm = {termNumber: 1, startDate: '2023-08-01', endDate: '2023-12-01'};
   });
 
-  test('testThatValidTermAddsToListWithoutErrors', async () => {
+  test('testThatValidTermPostAddsToList', async () => {
     // create a valid term in the database
-    const msg = createTerm(testTerm);
-    // expect there to be a success message
-    expect(msg.success).toBe('Term Added Successfully');
+    supertest(app).post('/term').send(testTerm).expect(201); // expect success: created
     // find the newly added term to ensure it exists in the database
-    const foundTerm = await Term.findByPk(msg.pk);
+    const foundTerm = await Term.findOne({where: {
+      termNumber: testTerm.termNumber,
+      startDate: testTerm.startDate,
+      endDate: testTerm.endDate}});
     // if the term does not exist, it will not be truthy; it will be null
     expect(foundTerm).toBeTruthy();
   });
 
-  test('testThatInvalidTermDoesNotSaveToListAndReturnsErrorMessage', async () => {
+  test('testThatInvalidTermPostDoesNotSaveToList', async () => {
     // store initial number of terms to compare against later
     const oldNumTerms = (await Term.findAll()).length;
     // change term number so that the term object is invalid
     testTerm.termNumber = 3;
     // attempt to create the term in the database
-    const msg = createTerm(testTerm);
-    // expect there to be error messages for the invalid fields
-    expect(msg.startDate).toBe('Term 3 must start in May');
-    expect(msg.endDate).toBe('Term 3 must end in May or June');
-    // expect there to be no success message
-    expect(msg.success).toBeFalsy();
+    supertest(app).post('/term').send(testTerm).expect('422'); // expect 422: unprocessable entity
     // since no term should have been added to the database, the number of terms should remain the same
     const newNumTerms = (await Term.findAll()).length;
     expect(newNumTerms).toBe(oldNumTerms);
@@ -46,7 +44,7 @@ describe('Terms in database', async () => {
     // create a new term to delete, then promptly delete it
     // testTerm does not have an ID, so use newTerm instead
     const newTerm = await Term.create(testTerm);
-    await deleteTerm(newTerm);
+    supertest(app).delete('/term').send(newTerm).expect(200); // expect 200: OK
 
     // If the term was deleted successfully, the number of terms in the database should
     // remain the same as it was before the 'create' statement
