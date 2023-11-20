7459d5a9239f754649e7926c92918820984293fc
Banman <banman9021@saskpolytech.ca>
2023-11-20 16:06:05 -0600

    Added functional tests to Term to ensure POSTs/DELETEs work on both empty & populated databases, implemented constants file - Dave

--- __tests__/functional/Term.func.js
+++ __tests__/functional/Term.func.js
@@ -2,6 +2,7 @@
 const Term = require('../../private/javascript/Term.js');
 const supertest = require('supertest');
 const app = require('../../app');
+const constants = require('../../private/javascript/constants');
 
 // this set of tests ensures that Term objects are handled within the database properly
 describe('Terms in database', () => {
@@ -11,21 +12,23 @@ describe('Terms in database', () => {
   beforeEach(async () => {
     // drop the table and re-create it
     await Term.sync({force: true});
-    testTerm = {termNumber: 1, startDate: '2023-08-01', endDate: '2023-12-01'};
+    testTerm = constants.validTerms[0];
   });
 
-  test('testThatValidTermPostAddsToList', async () => {
-    // create a valid term in the database
-    const res = await supertest(app).post('/term').send(testTerm).expect(201); // expect 201: created
-    console.log('response body:');
-    console.log(res.body);
-    // find the newly added term to ensure it exists in the database
-    const foundTerm = await Term.findOne({where: {
-      termNumber: testTerm.termNumber,
-      startDate: testTerm.startDate,
-      endDate: testTerm.endDate}});
-    // if the term does not exist, it will not be truthy; it will be null
-    expect(foundTerm).toBeTruthy();
+  test('testThatValidTermPostAddsToEmptyList', async () => {
+    // do the POST test starting with an empty database
+    await testPost(testTerm);
+  });
+
+  test('testThatValidTermPostAddsToPopulatedList', async () => {
+    // create a few valid terms in the database
+    for (const term of constants.validTerms) {
+      await Term.create(term);
+    }
+
+    // do the POST test now with more entries in the database
+    testTerm.startDate = '2023-09-01';
+    await testPost(testTerm);
   });
 
   test('testThatInvalidTermPostDoesNotSaveToList', async () => {
@@ -40,19 +43,19 @@ describe('Terms in database', () => {
     expect(newNumTerms).toBe(oldNumTerms);
   });
 
-  test('testThatValidTermDeleteRemovesFromList', async () => {
-    // store initial number of terms to compare against later
-    const oldNumTerms = (await Term.findAll()).length;
-    // create a new term to delete, then promptly delete it
-    // testTerm does not have an ID, so use newTerm instead
-    const newTerm = await Term.create(testTerm);
-    // delete the term
-    await supertest(app).delete('/term').send(newTerm).expect(200); // expect 200: OK
+  // TODO: rename this one to be DeletesLastItem, add one for populated list
+  test('testThatValidTermDeleteRemovesFromEmptyList', async () => {
+    // do the DELETE test without any Terms in the database
+    await testDelete(testTerm);
+  });
 
-    // If the term was deleted successfully, the number of terms in the database should
-    // remain the same as it was before the 'create' statement
-    const newNumTerms = (await Term.findAll()).length;
-    expect(newNumTerms).toBe(oldNumTerms);
+  test('testThatValidTermDeleteRemovesFromPopulatedList', async () => {
+    // create a few valid terms in the database
+    for (const term of constants.validTerms) {
+      await Term.create(term);
+    }
+    // do the DELETE test now that there are some Terms in the database
+    await testDelete(testTerm);
   });
 
   test('testThatInvalidTermDeleteDoesNotRemoveFromList', async () => {
@@ -115,3 +118,36 @@ describe('Terms in database', () => {
     expect(newNumTerms).toBe(oldNumTerms);
   });
 });
+
+/**
+ * This function tests POST requests on the Term router
+ * @param {Object} testTerm - The term to POST
+ */
+const testPost = async function(testTerm) {
+  const res = await supertest(app).post('/term').send(testTerm).expect(201); // expect 201: created
+
+  // find the newly added term in the database
+  // for this to work correctly, the router must set a parameter named 'id' using res.set()
+  const foundTerm = await Term.findOne({where: {id: parseInt(res.get('id'))}});
+  // if the term does not exist, it will not be truthy; it will be null
+  expect(foundTerm).toBeTruthy();
+};
+
+/**
+ * This function tests DELETE requests on the Term router
+ * @param {Object} testTerm - The term to DELETE
+ */
+const testDelete = async function(testTerm) {
+  // store initial number of terms to compare against later
+  const oldNumTerms = (await Term.findAll()).length;
+  // create a new term to delete, then promptly delete it
+  // testTerm does not have an ID, so use newTerm instead
+  const newTerm = await Term.create(testTerm);
+  // delete the term
+  await supertest(app).delete('/term').send(newTerm).expect(200); // expect 200: OK
+
+  // If the term was deleted successfully, the number of terms in the database should
+  // remain the same as it was before the 'create' statement
+  const newNumTerms = (await Term.findAll()).length;
+  expect(newNumTerms).toBe(oldNumTerms);
+};
--- fixtures/Term.fix.js
+++ fixtures/Term.fix.js
@@ -1,4 +1,5 @@
 const Term = require('../private/javascript/Term');
+const constants = require('../private/javascript/constants');
 
 /**
  * This clears the table for Term and then recreates the table
@@ -10,36 +11,9 @@ async function fillTermTable() {
   await Term.sync({force: true});
 
   // Fill with the six terms, with valid start and end dates
-  await Term.create({
-    termNumber: 1,
-    startDate: '2023-09-01',
-    endDate: '2023-12-01',
-  });
-  await Term.create({
-    termNumber: 4,
-    startDate: '2023-09-01',
-    endDate: '2023-12-01',
-  });
-  await Term.create({
-    termNumber: 2,
-    startDate: '2023-01-01',
-    endDate: '2023-04-01',
-  });
-  await Term.create({
-    termNumber: 5,
-    startDate: '2023-01-01',
-    endDate: '2023-04-01',
-  });
-  await Term.create({
-    termNumber: 3,
-    startDate: '2023-05-01',
-    endDate: '2023-05-31',
-  });
-  await Term.create({
-    termNumber: 6,
-    startDate: '2023-05-01',
-    endDate: '2023-05-31',
-  });
+  for (const term of constants.validTerms) {
+    await Term.create(term);
+  }
 }
 
 module.exports = fillTermTable;
