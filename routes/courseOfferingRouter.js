4b92f415a7472b2f3f45d62b6aa1236231423da6
Mason <rayburnharman7728@saskpolytech.ca>
2023-11-28 13:25:24 -0600

    started coding, quick router change  - Mason

--- routes/courseOfferingRouter.js
+++ routes/courseOfferingRouter.js
@@ -22,6 +22,13 @@ router.get('/', async function(req, res, next) {
   });
 });
 
+router.post('/', async function(req, res, next) {
+  createCourseOffering(req.body.)
+
+
+
+});
+
 /**
  * Creates a course offering in the database, returns course offering
  * @param createCO
