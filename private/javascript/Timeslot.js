203268593dcdab9dc416238399458f49c5272484
rayburnharman7728 <rayburnharman7728@saskpolytech.ca>
2024-01-04 13:33:07 -0600

    Created timeslot object - Mason

---
+++ private/javascript/Timeslot.js
@@ -0,0 +1,90 @@
+const {sequelize, DataTypes} = require('../../dataSource');
+const Course = require('../javascript/Course');
+const Term = require('../javascript/Term');
+const Instructor = require('../javascript/Instructor');
+const Program = require('../javascript/Program');
+const Room = require('../javascript/Classroom');
+
+/**
+ * This class stores objects that represent course offerings to be used in the CST Scheduler.
+ */
+const CourseOffering = sequelize.define('CourseOffering', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+    },
+
+    //Dates - YYYY-MM-DD
+    startDate: {
+        type: DataTypes.STRING,
+    },
+
+    endDate: {
+        type: DataTypes.STRING,
+    },
+
+    startTime: {
+        type: DataTypes.TIME,
+    },
+
+    endTime: {
+        type: DataTypes.TIME,
+    },
+
+    day: {
+        type: DataTypes.STRING,
+    },
+
+    group: {
+        type: DataTypes.STRING(1),
+        validate: {
+            isAlphanumeric: {
+                args: true,
+                msg: 'Course Offering group can only contain letters and numbers',
+            },
+            isUppercase: {
+                args: true,
+                msg: 'Course Offering group can only contain uppercase letters',
+            },
+            len: {
+                args: [0, 1],
+                msg: 'Course offering group can only be 0 or 1 character long',
+            },
+        },
+    },
+
+    termID: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+    },
+
+    programID: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+    },
+
+    instructorID: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+    },
+
+    courseID: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+    },
+
+    roomID: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+    },
+
+}, {
+    tableName: 'CourseOfferings',
+});
+
+
+module.exports = CourseOffering;
