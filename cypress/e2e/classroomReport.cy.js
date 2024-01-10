afd2f161b8ce64c90896c00ec65b39dc1f9b3ddf
Raven <hogan6587@saskpolytech.ca>
2024-01-10 14:53:03 -0600

    After spending several minutes meticulously tracing the root cause of a persistent bug, I finally realized it originated from erroneous import statements in our cy.js file. This challenging debugging session led to a breakthrough: the implementation of our first Cypress test. With a focus on efficiency and thoroughness, I carefully designed this test to perform as many checks as possible in a single run, aiming to maximize our testing coverage and minimize future issues

--- cypress/e2e/classroomReport.cy.js
+++ cypress/e2e/classroomReport.cy.js
@@ -1,22 +1,14 @@
-const Timeslot = require('../../private/javascript/Timeslot');
-const {sequelize} = require('../../datasource');
-const constants = require('../../constants');
-const Instructor = require('../../private/javascript/Instructor');
-
-
 describe('template spec', () => {
-  // beforeEach(async () => {
-  //   // drop the table and re-create it
-  //   await Timeslot.sync({force: true});
-  //   testTimeslot = {...constants.testConst.instructor1};
-  // });
-
-  it('passes', () => {
+  it('GenerateClassroomSchedule', () => {
     cy.visit('http://localhost:3000');
-
-    // add more steps
-    cy.contains('Reports');
-    cy.contains('Administration').click();
-    // cy.contains('Reports');
+    cy.scrollTo('MenuBar');
+    cy.get('MenuBar').click();
+    cy.contains('ClassRoom Report').click();
+    cy.get('btnGenerateSchedule').should('be.disabled')
+    cy.get('selectClassroom').select('Classroom 239B');
+    cy.get('btnGenerateSchedule').should('be.disabled');
+    cy.get('selectTerm').select('Term 5');
+    cy.get('btnGenerateSchedule').should('be.enabled');
+    cy.get('btnGenerateSchedule').click();
   });
 });
