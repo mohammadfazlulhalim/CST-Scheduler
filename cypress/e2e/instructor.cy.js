4e9d327e17089e6de461fc7493e6c19e6cc698c5
Mohammad <halim5471@saskpolytech.ca>
2024-01-11 15:08:29 -0600

    updated the UI testing according to the ATP

--- cypress/e2e/instructor.cy.js
+++ cypress/e2e/instructor.cy.js
@@ -1,8 +1,7 @@
 describe('Testing Instructor CRUD options', () => {
-  beforeEach(()=>{
-    cy.visit('http://localhost:3000');
-  });
-  const InsListingsPageHeader='body > div > div > div > h1';
+
+  const addNewInsModalHeader='#addModalLabel';
+  const insListingsPageHeader='body > div > div > div > h1';
   const tableFirstHeader ='body > div > div > div > table > thead > tr > th:nth-child(2)';
   const tableSecondHeader='body > div > div > div > table > thead > tr > th:nth-child(3)';
   const tableThirdHeader='body > div > div > div > table > thead > tr > th:nth-child(4)';
@@ -20,29 +19,46 @@ describe('Testing Instructor CRUD options', () => {
   const newInsOfficeNumInputBox='#cOffice';
   const newInsPhoneNumInputBox='#cPhone';
   const newInsEmailInputBox='#cEmail';
-  const CreateInstructorButton='#createInstructor';
+  const CreateInstructorButtonOnNewInsModal='#createInstructor';
   const insListFirstNameOnFirstColumn='#\\31 firstName';
   const insListEditButtonOnFirstColumn = '#\\31 edit';
+  const editModalHeader='#editModalLabel';
+  const firstTextBoxOnEditModal='#eFirstName';
+  const secondTextBoxOnEditModal ='#eLastName';
+  const saveButtonOnEditModal ='#editInstructor';
+  const insListDeleteButtonOnFirstColumn ='#\\31 delete' ;
+  const confirmDeleteButtonOnDeleteModal='#deleteInstructor';
+  const insListFirstNameOnFirstColumnAfterDeletion = '#\\32 firstName'
+
+
 
   const navMenuSelector ='body>open>nav';
   const navMenuItem= 'body>nav>menu';
 
-  it('Instructor listings page is shown', () => {
-    // TODO check the tables do not exist
-
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000');
     cy.get(navMenuSelector).trigger('mouseover', { /* TODO find the option I must send mouse over*/});
     cy.get(navMenuItem).click();
-    cy.get(InsListingsPageHeader).should('have.text', 'Instructor List');
+  });
+
+  it('Test Instructor Lists are Shown Inside Instructor Page  ', () => {
+    // TODO check the tables do not exist
+    cy.get(insListingsPageHeader).should('have.text', 'Instructor List');
     cy.get(tableFirstHeader).should('have.text', 'First Name');
     cy.get(tableSecondHeader).should('have.text', 'Last Name');
     cy.get(tableThirdHeader).should('have.text', 'Office Number');
     cy.get(tableFourthHeader).should('have.text', 'Phone Number');
     cy.get(tableFifthHeader).should('have.text', 'Email');
     cy.get(tableSixthHeader).should('have.text', 'Action');
+    cy.get(insListEditButtonOnFirstColumn).should ('have.text', 'Edit');
+    cy.get(insListDeleteButtonOnFirstColumn).should ('have.text', 'Delete');
+    cy.get(createInsButton).should ('have.text', 'Add New Instructor')
+
   });
-  it('Create New Instructor modal is shown after clicking ADD NEW INSTRUCTOR Button', () => {
-    cy.get('body > div > div > div > button').click();
-    cy.get('#addModalLabel').should('have.text', 'Create New Instructor');
+
+  it('Test Create New Instructor Modal ', () => {
+    cy.get(createInsButton).click();
+    cy.get(addNewInsModalHeader).should('have.text', 'Create New Instructor');
     cy.get(addInsModalFirstTextBoxLabel).should('have.text', 'First Name:');
     cy.get(addInsModalSecondTextBoxLabel).should('have.text', 'Last Name:');
     cy.get(addInsModalThirdTextBoxLabel).should('have.text', 'Office Number:');
@@ -51,38 +67,35 @@ describe('Testing Instructor CRUD options', () => {
   });
 
 
-  it('Add information inside New Instructor modal', () => {
-    cy.visit('http://localhost:3000/instructor');
+  it('Test Adding Information of New Instructor ', () => {
     cy.get(createInsButton).click();
     cy.get(newInsFirstNameInputTextBox).type('Ben');
     cy.get(newInsLastNameInputTextBox).type('Benson');
     cy.get(newInsOfficeNumInputBox).type('223A.1');
     cy.get(newInsPhoneNumInputBox).type('(123)-456-9655');
     cy.get(newInsEmailInputBox).type('benson@saskpolytech.ca');
-    cy.get(CreateInstructorButton).click();
+    cy.get(CreateInstructorButtonOnNewInsModal ).click();
     cy.get(insListFirstNameOnFirstColumn).should('have.text', 'Ben');
   });
 
-  it('Edit information inside Instructor List Page ', () => {
+  it('Test Edit Instructor Information', () => {
     // Test Edit instructor information
-    cy.visit('http://localhost:3000/instructor');
     cy.get(insListEditButtonOnFirstColumn).click();
-    cy.get('#editModalLabel').should('have.text', 'Edit Existing Instructor');
-    cy.get('#eFirstName').clear();
-    cy.get('#eFirstName').type('John');
-    cy.get('#eLastName').clear();
-    cy.get('#eLastName').type('Doe');
-    cy.get('#editInstructor').click();
-    cy.get('#\\31 firstName').should('have.text', 'John');
+    cy.get(editModalHeader).should('have.text', 'Edit Existing Instructor');
+    cy.get(firstTextBoxOnEditModal).clear();
+    cy.get(firstTextBoxOnEditModal).type('John');
+    cy.get(secondTextBoxOnEditModal).clear();
+    cy.get(secondTextBoxOnEditModal).type('Doe');
+    cy.get(saveButtonOnEditModal).click();
+    cy.get(insListFirstNameOnFirstColumn).should('have.text', 'John');
   });
 
 
-  it('Delete information inside Instructor List Page ', () => {
-    cy.visit('http://localhost:3000/instructor');
-    cy.get('#\\31 delete').click();
+  it('Test Delete Instructor Button  ', () => {
+    cy.get(insListDeleteButtonOnFirstColumn).click();
     // cy.get('#deleteModal > div > div').should('have.text', 'Delete Instructor');
 
-    cy.get('#deleteInstructor').click();
-    cy.get('#\\32 firstName').should('have.text', 'Coralee');
+    cy.get(confirmDeleteButtonOnDeleteModal).click();
+    cy.get(insListFirstNameOnFirstColumnAfterDeletion).should('have.text', 'Coralee');
   });
 });
