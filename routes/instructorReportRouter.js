14bd0dd9684592261dc9130d8369d8cd79b9698b
Ritish Rawat <rawat0182@saskpolytech.ca>
2024-01-15 15:06:02 -0600

    Ritish & Christeen 2hrs - layouts.hbs added navbar - added report table stub and basic data transfer from instructorReportRouter.js to instructorReport.hbs on the GET - still needs that modal form...

--- routes/instructorReportRouter.js
+++ routes/instructorReportRouter.js
@@ -7,12 +7,35 @@ const router = express.Router();
  *
  */
 router.get('/', function(req, res, next) {
+  let program="";
+  let dateGenerated= new Date();
+  const monthArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+
   const instructor={
-    lastName: 'Benson',
-    firstName: 'Ben',
+    lastName: 'BensonBenson',
+    firstName: 'Onisheknooo',
   };
+
+  const t={
+    c:'CSEC280B',
+    roomNum: 244,
+    term: 2
+  };
+
+
+  if (t.term > 3)
+  {
+    program = "CST 2"
+  }
+  else{
+    program ="CST 1"
+  }
   res.render('instructorReport', {
     instructor,
+    timeslot: t,
+    programYear: program,
+    dateGen: dateGenerated.getDate()+'-'+monthArray[dateGenerated.getMonth()]+'-'+dateGenerated.getFullYear(),
   });
 });
 
