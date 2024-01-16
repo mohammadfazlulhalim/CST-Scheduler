741833d041c375bd3f7f1cd6dbb2329f5dc7a7c8
shlimoon9785 <shlimoon9785@saskpolytech.ca>
2024-01-16 13:48:42 -0600

    Christeen and Ritish coded modal and connected DB to modal inputs. 2 hours

--- routes/instructorReportRouter.js
+++ routes/instructorReportRouter.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Instructor = require('../private/javascript/Instructor');
+const Term = require('../private/javascript/Term');
+const {sequelize} = require('../dataSource');
 
 
 /**
  * Processing GET request for rendering the instructor report page.
  *
  */
-router.get('/', function(req, res, next) {
-  let program="";
-  let dateGenerated= new Date();
+router.get('/', async function(req, res, next) {
+  let program='';
+  const dateGenerated= new Date();
   const monthArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  let instructorList;
+  let termList;
 
+  try {
+    instructorList= await Instructor.findAll({order: ['lastName']});
+  } catch (err) {
+    instructorList = undefined;
+  }
+  try {
+    termList= await Term.findAll({order: ['termNumber']});
+  } catch (err) {
+    termList = undefined;
+  }
 
   const instructor={
     lastName: 'BensonBenson',
@@ -18,24 +33,24 @@ router.get('/', function(req, res, next) {
   };
 
   const t={
-    c:'CSEC280B',
+    c: 'CSEC280B',
     roomNum: 244,
-    term: 2
+    term: 2,
   };
 
 
-  if (t.term > 3)
-  {
-    program = "CST 2"
-  }
-  else{
-    program ="CST 1"
+  if (t.term > 3) {
+    program = 'CST 2';
+  } else {
+    program ='CST 1';
   }
   res.render('instructorReport', {
-    // instructor,
+    instructor,
     timeslot: t,
     programYear: program,
     dateGen: dateGenerated.getDate()+'-'+monthArray[dateGenerated.getMonth()]+'-'+dateGenerated.getFullYear(),
+    instructorList,
+    termList,
   });
 });
 
