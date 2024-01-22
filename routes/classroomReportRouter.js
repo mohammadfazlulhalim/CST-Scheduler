dd6677321f318e0cafc1c143879bfdd5328f42f1
Jonah <dinh3453@saskpolytech.ca>
2024-01-22 14:59:55 -0600

    We shifted our efforts towards understanding the 'what happens after' process, once we have the term and date objects in hand, aiming to construct our timeslot array; however, we encountered significant challenges in making headway, primarily due to the intricate and multi-layered complexity inherent in the required looping structures.

--- routes/classroomReportRouter.js
+++ routes/classroomReportRouter.js
@@ -22,96 +22,81 @@ router.post('/', async (req, res, next) => {
   await addAssociations();
   await createAllTables(false);
 
-
-  //TODO - switch to req.body when the modal is complete
-
   // const hardTerm = await Term.findOne({where: {startDate: testConst.term1.startDate}}); //change to ID later
   // const hardProg = await Program.findOne({where: {programAbbreviation: testConst.program1.programAbbreviation}}); //change to id later
   // const  realTerm = await term.findOne({where: {termNumber: res.body.termSelect}});
-  // let TimeSlots = generateSchedule(realTerm.startDate, realTerm.endDate, realClassroom);
 
+  const TimeSlots = generateSchedule(realTerm.startDate, realTerm.endDate, realClassroom);
 
-  const hardGroups = 2;
-  let groupArray = [];
+  const ScheduleArray = [[], [], [], [], [], [], [], []];
 
-  const GROUP_LETTERS = ['A', 'B', 'C', 'D'];
   const DAYS = [0, 1, 2, 3, 4, 5];
   const TIMES = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00'];
 
-  // const timeslotMatrix = [[], [] , [], [], []];
-  let timeslotArray = new Array(hardGroups);
-  const COArray = new Array(hardGroups);
-  const groupLetters = new Array(hardGroups);
-
-
-  for (let i = 0; i < hardGroups; i++) {
-    groupArray.push({
-      timeslotMatrix: [[], [], [], [], [], [], [], []], //outer array is times, each inner array is days
-      COArray: new Array(hardGroups),
-      groupLetter: GROUP_LETTERS[i],
-    });
-
-    // Creating the 2D array with empty values
-    for (t in TIMES) {
-      for (d in DAYS) {
-        let timeOb = null;
-        if (d == 0) {
-          // just storing the time
-          // TODO converting the time
-          timeOb = TIMES[t];
-        }
-        groupArray[i].timeslotMatrix[t][d] = {
-          hasObj: false,
-          tTime: t,
-          tDays: d,
-          timeslot: timeOb,
-        };
-      }
-    }
-
-    try {
-      timeslotArray = await Timeslot.findAll({
-        where: {
-          group: GROUP_LETTERS[i],
-          ProgramId: hardProg.id,
-          TermId: hardTerm.id,
-        },
-      });
-      groupArray[i].COArray = await CourseOffering.findAll({
-        where: {
-          group: GROUP_LETTERS[i],
-          ProgramId: hardProg.id,
-          TermId: hardTerm.id,
-        },
-      });
-    } catch (error) {
-      // console.log('Error is: ' + error);
-    }
-
-
-    //mapping each timeslot in this group to the matrix
-    for (const tSlot of timeslotArray) {
-      const formattedTSlot = await formatCellInfo(tSlot);
-      groupArray[i].timeslotMatrix[TIMES.indexOf(tSlot.startTime)][tSlot.day].timeslot = formattedTSlot;//outer array is days, each inner array is times
-    }
-    groupLetters[i] = GROUP_LETTERS[i];
+  // eslint-disable-next-line guard-for-in
+  for (ts in Timeslots) {
+    ScheduleArray[TIMES.indexOf(ts.startTime)][DAYS.indexOf(ts.day)] = ts;
   }
 
+  // const timeslotMatrix = [[], [] , [], [], []];
+  let timeslotArray = new Array(hardGroups);
+  const groupLetters = new Array(hardGroups);
+
+  ScheduleArray.push({
+    timeslotMatrix: [[], [], [], [], [], [], [], []], // outer array is times, each inner array is days
+    COArray: new Array(hardGroups),
+    groupLetter: GROUP_LETTERS[i],
+  });
+
+  // Creating the 2D array with empty values
+  for (t in TIMES) {
+    for (d in DAYS) {
+      let timeOb = null;
+      if (d == 0) {
+        // just storing the time
+        // TODO converting the time
+        timeOb = TIMES[t];
+      }
+      groupArray[i].timeslotMatrix[t][d] = {
+        hasObj: false,
+        tTime: t,
+        tDays: d,
+        timeslot: timeOb,
+      };
+    }
+  }
+
+  try {
+    timeslotArray = await Timeslot.findAll({
+      where: {
+        classroom: GROUP_LETTERS[i],
+        ProgramId: hardProg.id,
+        TermId: hardTerm.id,
+      },
+    });
+  } catch (error) {
+    // console.log('Error is: ' + error);
+  }
+
+
+  // mapping each timeslot in this group to the matrix
+  for (const tSlot of timeslotArray) {
+    const formattedTSlot = await formatCellInfo(tSlot);
+    groupArray[i].timeslotMatrix[TIMES.indexOf(tSlot.startTime)][tSlot.day].timeslot = formattedTSlot;// outer array is days, each inner array is times
+  }
+  groupLetters[i] = GROUP_LETTERS[i];
+
+
   res.render('schedule', {
-    groups: groupLetters,
-    groupArray,
+
     DAYS,
     TIMES,
   });
-
-
 });
 
 function generateSchedule(startDate, endDate, classroom) {
   const timeslots = timeslot.findAll({
-    where: {
-
-    },
+    where: {},
   });
 
   return timeslots;
@@ -125,7 +110,6 @@ async function formatCellInfo(tSlot) {
   insObj = await tSlot.getInstructor();
 
   return cObj.courseCode + '    ' + insObj.lastName;
-
 }
 
 
