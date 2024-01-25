fceb7bc612ce491ee481cee51ebd4a2210aab042
Mohammad <halim5471@saskpolytech.ca>
2024-01-24 22:36:58 -0600

    Added 3 more UI testing

--- cypress/e2e/instructor.cy.js
+++ cypress/e2e/instructor.cy.js
@@ -1,5 +1,7 @@
-
+/*The following tests are created to check functionality of the app to create, update and delete Instructors */
 describe('Testing Instructor CRUD options', () => {
+
+  //these are the selectors for HTML elements
   const addNewInsModalHeader='#addModalLabel';
   const insListingsPageHeader='body > div > div > div > h1';
   const tableFirstHeader ='body > div > div > div > table > thead > tr > th:nth-child(1)';
@@ -20,22 +22,18 @@ describe('Testing Instructor CRUD options', () => {
   const newInsPhoneNumInputBox='#cPhoneNum';
   const newInsEmailInputBox='#cEmail';
   const CreateInstructorButtonOnNewInsModal='#createInstructor';
-  const insListFirstNameOnFirstColumn='#\\31 firstName';
   const insListEditButtonOnFirstColumn = '#\\36 edit';
   const editModalHeader='#editModalLabel';
   const firstTextBoxOnEditModal='#eFirstName';
   const secondTextBoxOnEditModal ='#eLastName';
   const saveButtonOnEditModal ='#editInstructor';
-  const insListDeleteButtonOnFirstColumn ='#\\31 delete';
   const insListDeleteButtonOnFirstColumnForDeleteTest ='#\\36 delete';
   const confirmDeleteButtonOnDeleteModal='#deleteInstructor';
-  const insListFirstNameOnFirstColumnAfterDeletion = '#\\32 firstName';
-  const insListFirstNameOnFirstColumnAfterCreation='#\\36 firstName'
-  const insListFirstNameOnFirstColumnAfterEditing='#\\36 firstName'
   const navMenuSelector ='#navbarColor04 > ul > li:nth-child(2) > a';
   const navMenuItem= '#navbarColor04 > ul > li:nth-child(2) > div > a:nth-child(3)';
 
 
+/* Navigating to our expected page "http://localhost:3000/instructor"*/
 
    beforeEach(()=>{
     cy.visit('http://localhost:3000');
@@ -43,8 +41,8 @@ describe('Testing Instructor CRUD options', () => {
     cy.get(navMenuItem, {timeout:10000}).click();
   });
 
-  it('testInstructorListsAreShownInsideInstructorPage  ', () => {
-    // TODO check the tables do not exist
+   /* checking the availability of instructor lists headers*/
+  it('testInstructorListsAreShownUnderCertainHeaders  ', () => {
     cy.get(insListingsPageHeader).should('have.text', 'Instructor Listings');
     cy.get(tableFirstHeader).should('have.text', 'First Name');
     cy.get(tableSecondHeader).should('have.text', 'Last Name');
@@ -52,11 +50,25 @@ describe('Testing Instructor CRUD options', () => {
     cy.get(tableFourthHeader).should('have.text', 'Phone Number');
     cy.get(tableFifthHeader).should('have.text', 'Email');
     cy.get(tableSixthHeader).should('have.text', 'Actions');
-    // cy.get(insListEditButtonOnFirstColumn).should('have.text', 'Edit');
-    // cy.get(insListDeleteButtonOnFirstColumn).should('have.text', 'Delete');
-    // cy.get(createInsButton).should('have.text', 'Add New Instructor');
+
   });
 
+  /* Test to confirm the availability of instructor's name according to the fixture data*/
+  it ('testAvailabilityOfAllInstructorInfo',()=>{
+    cy.contains('Bryce').should('exist');
+    cy.contains('Ernesto').should('exist');
+    cy.contains('Rick').should('exist');
+    cy.contains('Coralee').should('exist');
+    cy.contains('Ron').should('exist');
+  })
+
+  /* Test to confirm the unavailability of new instructor's name (to be created/ added)  according to the fixture data*/
+  it ('testUnavailabilityOfNewInstructorInfo',()=>{
+    cy.contains('Ben').should('not.exist');
+    cy.contains('Benson').should('not.exist');
+  })
+
+/* Test  "Add New Instructor" button navigates us to "Create New Instructor" modal, and it has required input boxes */
   it('testCreateNewInstructorModal ', () => {
     cy.get(createInsButton, {timeout:10000}).click();
     cy.get(addNewInsModalHeader).should('have.text', 'Create New Instructor');
@@ -68,19 +80,20 @@ describe('Testing Instructor CRUD options', () => {
   });
 
 
+  /* Test to  create a new instructor Ben Benson and confirm the availability inside Instructor Listings*/
   it('testAddingInformationOfNewInstructor ', () => {
     cy.get(createInsButton, {timeout:10000}).click();
-    cy.get(newInsFirstNameInputTextBox, {timeout:10000}).type('Ben');
-    cy.get(newInsLastNameInputTextBox, {timeout:10000}).type('Benson');
+    cy.get(newInsFirstNameInputTextBox, {timeout:50000}).type('Ben');
+    cy.get(newInsLastNameInputTextBox,).type('Benson');
     cy.get(newInsOfficeNumInputBox, {timeout:10000}).type('223A.1');
     cy.get(newInsPhoneNumInputBox, {timeout:10000}).type('(123)-456-9655');
     cy.get(newInsEmailInputBox, {timeout:10000}).type('benson@saskpolytech.ca');
     cy.get(CreateInstructorButtonOnNewInsModal, {timeout:10000} ).click();
-    cy.get(insListFirstNameOnFirstColumnAfterCreation).should('have.text', 'Ben');
+    cy.contains('Ben').should('exist');
   });
 
+  /* Test to  edit instructor Ben Benson's name and confirm the availability of edited instructor inside Instructor Listings*/
   it('testEditInstructorInformation', () => {
-    // Test Edit instructor information
     cy.get(insListEditButtonOnFirstColumn, {timeout:10000}).click();
     cy.get(editModalHeader).should('have.text', 'Edit Existing Instructor');
     cy.get(firstTextBoxOnEditModal, {timeout:10000}).clear();
@@ -88,17 +101,15 @@ describe('Testing Instructor CRUD options', () => {
     cy.get(secondTextBoxOnEditModal, {timeout:10000}).clear();
     cy.get(secondTextBoxOnEditModal, {timeout:10000}).type('Doe');
     cy.get(saveButtonOnEditModal, {timeout:10000}).click();
-    cy.get(insListFirstNameOnFirstColumnAfterEditing, {timeout:10000}).should('have.text', 'John');
+    cy.contains('John').should('exist');
   });
 
-
+  /* Test to  delete instructor John's information and confirm the unavailability of deleted instructor inside Instructor Listings*/
   it('testDeleteInstructorButton  ', () => {
 
     cy.get(insListDeleteButtonOnFirstColumnForDeleteTest, {timeout:10000}).click();
-    // cy.get('#deleteModal > div > div').should('have.text', 'Delete Instructor');
-
     cy.get(confirmDeleteButtonOnDeleteModal, {timeout:10000}).click();
+    cy.contains('John').should('not.exist');
 
-    cy.get(insListFirstNameOnFirstColumnAfterDeletion).should('have.text', 'Bryce');
   });
 });
