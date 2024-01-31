d623d731532f7247370c0856c677265236a6a00d
Lysholm <lysholm9452@saskpolytech.ca>
2024-01-31 14:48:23 -0600

    Added more details to UI tests, all that remains is checking sort order of dropdowns and resetting database -Brandon

--- cypress/e2e/courseOffering.cy.js
+++ cypress/e2e/courseOffering.cy.js
@@ -10,7 +10,7 @@ it('testThatCourseOfferingHasAssociations', () => {
   cy.contains('Add New Course Offering').click();
   cy.get('#addModal').should('be.visible');
 
-  // TODO: Check sort orders on all associated fields before starting
+  // TODO: Check sort orders on all associated fields before starting - See instructor.cy.js
 
   cy.get('#createCO').should('be.disabled');
   cy.get('#cName').type('Mathematics of Computation');
@@ -24,7 +24,7 @@ it('testThatCourseOfferingHasAssociations', () => {
   cy.get('#cCourse').select('MATH282');
   cy.get('#createCO').should('be.disabled');
   // Associated field
-  cy.get('#cInstructor').select('Grezsina')
+  cy.get('#cInstructor').select('Grzesina')
   cy.get('#createCO').should('be.disabled');
   // Last associated field
   cy.get('#cProgam').select('CST');
@@ -33,22 +33,32 @@ it('testThatCourseOfferingHasAssociations', () => {
   cy.get('#createCO').click();
 
   // created, now check that it exists
-  // TODO get an unknown row, and check it contains all the data needed
+  // TODO get an unknown row, and check it contains all the data needed - figure out which row it is and select it
+  const newRow = '#tableBody > tr:nth-child(?)'
+  cy.get(newRow + 'td:nth-child(1)').contains('Mathematics of Computation');
+  cy.get(newRow + 'td:nth-child(2)').contains('2023-09-01');
+  cy.get(newRow + 'td:nth-child(3)').contains('2023-12-15');
+  cy.get(newRow + 'td:nth-child(4)').contains('A');
+  cy.get(newRow + 'td:nth-child(5)').contains('MATH282');
+  cy.get(newRow + 'td:nth-child(6)').contains('4-2023');
+  cy.get(newRow + 'td:nth-child(7)').contains('Grzesina');
+  cy.get(newRow + 'td:nth-child(8)').contains('CST');
 
-  // TODO select a specific row not knowing ID, but knowing its contents
-  // TODO: click edit button
 
+  cy.get('2edit').click();
   cy.get('#editModal').should('be.visible');
 
   // TODO: Check sort orders on all associated fields before starting
 
   // Changing associated fields
-  cy.get('eTerm').select('2023-2');
+  cy.get('eTerm').select('4-2023');
   cy.get('eInstructor').select('Barrie');
   cy.get('eProgram').select('CNT');
   cy.get('#editCO').click();
 
-  // TODO: Check that it changed all fields
+  cy.get('2Term').contains('2-2023');
+  cy.get('2Instructor').contains('Barrie');
+  cy.get('2Program').contains('CNT');
 
 
   //TODO revert all changes at the end back to how they started
