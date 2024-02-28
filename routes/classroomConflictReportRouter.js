aca22c75b14d9c55df52f29d62a080d5ae93300c
Halim <halim5471@saskpolytech.ca>
2024-02-28 14:21:12 -0600

    added code stubs inside the class conflict routers

---
+++ routes/classroomConflictReportRouter.js
@@ -0,0 +1,30 @@
+
+/**
+ * Post Handler for classroom conflict report router
+ * @param req
+ * @param res
+ * @param next
+ */
+function routerPost(req, res, next){
+
+}
+
+/**
+ * Get Handler for classroom conflict report router
+ * @param req
+ * @param res
+ * @param next
+ */
+function routerGet(req, res, next){
+
+}
+
+
+/**
+ * This function helps to finding and collecting  timeslots which
+ * are experiencing class conflicts
+ * @param timeSlots, classroom, term
+ */
+function checkForConflict(timeSlots, classroom, term){
+
+}
