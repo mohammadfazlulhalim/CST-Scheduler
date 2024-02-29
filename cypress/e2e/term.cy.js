74cc77317a1fa78295f9cd274611dea3c499deef
Lysholm <lysholm9452@saskpolytech.ca>
2024-02-29 13:52:08 -0600

    Added more detailed testing on the Course Offerings being created using placeholder selectors -Brandon

--- cypress/e2e/term.cy.js
+++ cypress/e2e/term.cy.js
@@ -37,9 +37,38 @@ it('testThatTermAutogeneratesCourseOfferings', () => {
     cy.get('#createCOInstructor > option:nth-child(' + nChild + ')').should('have.text', programList[i]);
   }
 
-  // Need CSS handlers made to change the form to ATP values
+  // expected number of entries
+  const numEntries =8;
+  const expectedNames = ['Hardware', 'Seminar'];
+  const expectedGroup = ['A', 'B', 'C', 'D'];
+  const expectedCourse = ['Hardware', 'Seminar'];
+  const expectedInstructor = ['Ben Benson', 'Ron New'];
 
-  // TODO: Write tests for CO modal once CSS selectors and HTMl format has been finalized
+  // checking that it autofilled correctly
+  for (let i =0; i< numEntries; i++) {
+    const nChild = i+1;
+    const keyBinary = (i/2).floor();
+    const keyGroup = (i/4).floor();
+    cy.get('ph-off' + nChild + 'ph-name').should('have.value', expectedNames[keyBinary]);
+    cy.get('ph-off' + nChild + 'ph-start').should('have.value', '2024-05-03');
+    cy.get('ph-off' + nChild + 'ph-end').should('have.value', '2024-06-02');
+    cy.get('ph-off' + nChild + 'ph-group').should('have.value', expectedGroup[keyGroup]);
+    cy.get('ph-off' + nChild + 'ph-course').should('have.value', expectedNames[keyBinary]);
+    cy.get('ph-off' + nChild + 'ph-instructor').should('have.value', expectedInstructor[keyBinary]);
+    cy.get('ph-off' + nChild + 'ph-program').should('have.value', 'CST');
+  }
+
+  // Date change
+  cy.get('ph-off' + 1 + 'ph-start').type('2024-05-05');
+  cy.get('ph-off' + 1 + 'ph-end').type('2024-05-29');
+  // Name change
+  cy.get('ph-off' + 2 + 'ph-name').type('The Seminar');
+  // Instructor change
+  cy.get('ph-off' + 3 + 'ph-instructor').select('Bryce Barrie');
+  // Program change
+  cy.get('ph-off' + 4 + 'ph-program').select('CNT');
+  // Skip
+  cy.get('ph-off' + 5 + 'ph-auto').check();
 
   // Check that new term is added:
   cy.get('#tableBody > tr:nth-child(1)').should('have.text', '\n 3 \n 2024-05-03 \n 2024-06-02');
@@ -54,6 +83,18 @@ it('testThatTermAutogeneratesCourseOfferings', () => {
   // const newRow2 = '#tableBody > tr:nth-child(21)';
   // cy.get(newRow2).should('have.text',
   //   '\n                    Mathematics of Computation\n                    A\n                    MATH282\n                    2023-5\n                    2023-01-01\n                    2023-04-01\n                    Bryce Barrie\n                    CNT\n                    \n                        Edit\n                        \n                        Delete\n                        \n                    \n                ');
+  const newCOs = [
+    'Hardware \n A \n COHS190 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ben Benson \n CST',
+    'Hardware \n B \n COHS190 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ben Benson \n CST',
+    'Hardware \n C \n COHS190 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ben Benson \n CST',
+    'Hardware \n D \n COHS190 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ben Benson \n CST',
+    'Seminar \n A \n SEM283 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ron New \n CST',
+    'Seminar \n B \n SEM283 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ron New \n CST',
+    'Seminar \n C \n SEM283 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ron New \n CST',
+    'Seminar \n D \n SEM283 \n 2024-3 \n 2024-05-03 \n 2024-06-02 \n Ron New \n CST',
+  ];
+
+  // Calculate that one was skipped by calculating out total number of rows
   // TODO delete newly created COs
 
   // Deleting the Term
