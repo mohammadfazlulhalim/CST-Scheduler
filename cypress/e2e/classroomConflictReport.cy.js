35d4778d498e755f03ec3cde2d9abd33aa113d47
Ritish Rawat <rawat0182@saskpolytech.ca>
2024-02-29 15:00:12 -0600

    - added loop to process header contents in classroom conflict table (classroomConflictReport.cy.js)
    (Ritish&Mohammad)

--- cypress/e2e/classroomConflictReport.cy.js
+++ cypress/e2e/classroomConflictReport.cy.js
@@ -1,30 +1,49 @@
+
+
 it('testClassroomConflictsFound ', () => {
-    // Opens main page - and go to Classroom Conflict Report Page
-    cy.visit('localhost:3000');
-    cy.contains('Reports').click();
-    cy.get ('.nav-item.dropdown .dropdown-menu a[href="/classroomConflictReport"]').click();
-    cy.get('#classroomConflictModal').should('be.visible');
-    // Assert that the URL has changed to the classroom Conflict Report page
-    cy.url().should('include', '/classroomConflictReport');
-
-    // Sort orders are checked in the other test, as one giant test was not running smoothly
-
-    // Check that Enter button is disabled
-    cy.get('#modalSubmit').should('be.disabled');
-
-    // Check that classroom field can be entered
-    cy.contains('Classroom');
-    cy.get('#classroomSelect').select('239B');
-
-    cy.get('#modalSubmit').should('be.disabled');
-
-    cy.get('#modalSubmit').should('not.be.disabled');
-    cy.get('#modalSubmit').click();
-
-    cy.get('#classroomConflictModal').should('be.hidden');
+  // Opens main page - and go to Classroom Conflict Report Page
+  cy.visit('localhost:3000');
+  cy.contains('Reports').click();
+  cy.get('.nav-item.dropdown .dropdown-menu a[href="/classroomConflictReport"]').click();
+  cy.get('#classroomConflictModal').should('be.visible');
+  // Assert that the URL has changed to the classroom Conflict Report page
+  cy.url().should('include', '/classroomConflictReport');
 
 
+  // Check that Enter button is disabled
+  cy.get('#modalSubmit').should('be.disabled');
 
+  // Check that classroom field can be entered
+  cy.contains('Classroom');
+  cy.get('#classroomSelect').select('239B');
+  cy.get('#modalSubmit').should('not.be.disabled');
+  cy.get('#modalSubmit').click();
+
+  // modal for form disappears after submit button is clicked and sends a POST to the router
+  cy.get('#classroomConflictModal').should('be.hidden');
+
+  cy.get('h1').contains('Classroom Conflicts');
+
+  const headingsForEachClassroomReportTable = ['Term', 'Course Code', 'Weekday', 'Start Time', 'End Time', 'Instructor'];
+
+  // cy.get('#div239A table thead:first-child').each( (element, index, $list) => {
+  //   // either place
+  // } );
+
+  // cy.get('#239aTable thead:first-child').should('have.text', headingsForEachClassroomReportTable[0]);
+  // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[1]);
+  // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[2]);
+  // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
+  // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
+
+  for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
+    cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+  }
+
+  // TODO determine if its better to use { [], [], ...} or [ [], [], ... )
+  const expectedContent = [
+    ['2023-4', 'COOS291', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
+  ];
 
 
 });
