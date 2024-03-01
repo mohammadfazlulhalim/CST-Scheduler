5bbf083d64fb90eac48c797134a05503de2611f3
Mohammad <halim5471@saskpolytech.ca>
2024-03-01 12:24:40 -0600

    added the classroom conflict report cypress tests for all class rooms

--- cypress/e2e/classroomConflictReport.cy.js
+++ cypress/e2e/classroomConflictReport.cy.js
@@ -1,6 +1,9 @@
 
+describe( 'classroom conflict report page', ()=>{
+  const headingsForEachClassroomReportTable = ['Term', 'Course Code', 'Weekday', 'Start Time', 'End Time', 'Instructor'];
 
-it('testClassroomConflictsFound ', () => {
+
+it('testLoadClassroomConflictReport ', () => {
   // Opens main page - and go to Classroom Conflict Report Page
   cy.visit('localhost:3000');
   cy.contains('Reports').click();
@@ -9,41 +12,113 @@ it('testClassroomConflictsFound ', () => {
   // Assert that the URL has changed to the classroom Conflict Report page
   cy.url().should('include', '/classroomConflictReport');
 
+});
 
-  // Check that Enter button is disabled
-  cy.get('#modalSubmit').should('be.disabled');
+  it('testShowPageHeading', () => {
+    //go directly to classroomConflictReport form
+    cy.visit('localhost:3000/classroomConflictReport');
+    // Check that Enter button is disabled
+    cy.get('#modalSubmit').should('be.disabled');
+    // Check that classroom field can be entered
+    cy.contains('Classroom');
+    cy.get('#classroomSelect').select('239A');
+    // Check that Enter button is enabled
+    cy.get('#modalSubmit').should('not.be.disabled');
+    cy.get('#modalSubmit').click();
 
-  // Check that classroom field can be entered
-  cy.contains('Classroom');
-  cy.get('#classroomSelect').select('239B');
-  cy.get('#modalSubmit').should('not.be.disabled');
-  cy.get('#modalSubmit').click();
+    // modal for form disappears after submit button is clicked and sends a POST to the router
+    cy.get('#classroomConflictModal').should('be.hidden');
 
-  // modal for form disappears after submit button is clicked and sends a POST to the router
-  cy.get('#classroomConflictModal').should('be.hidden');
+    //Check header for classroom conflict page
+    cy.get('h1').contains('Classroom Conflicts');
 
-  cy.get('h1').contains('Classroom Conflicts');
+  });
 
-  const headingsForEachClassroomReportTable = ['Term', 'Course Code', 'Weekday', 'Start Time', 'End Time', 'Instructor'];
+  it('testCheckingRoomNumberElementForSpecificRoomNumber',()=>{
 
-  // cy.get('#div239A table thead:first-child').each( (element, index, $list) => {
-  //   // either place
-  // } );
+    //go directly to classroomConflictReport form page and submit the classroom info on the form
+    cy.visit('localhost:3000/classroomConflictReport');
+    cy.get('#classroomSelect').select('239A');
+    cy.get('#modalSubmit').click();
 
-  // cy.get('#239aTable thead:first-child').should('have.text', headingsForEachClassroomReportTable[0]);
-  // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[1]);
-  // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[2]);
-  // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
-  // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
+    //check room number element
+    cy.get('h2').contains('Room Number: 239A');
 
-  for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
-    cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
-  }
+  });
 
-  // TODO determine if its better to use { [], [], ...} or [ [], [], ... )
-  const expectedContent = [
-    ['2023-4', 'COOS291', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
-  ];
+  it ('testTableHeaderElementsForSpecificRoomNumber', ()=>{
+    //go directly to classroomConflictReport form page and submit the classroom info on the form
+    cy.visit('localhost:3000/classroomConflictReport');
+    cy.get('#classroomSelect').select('239A');
+    cy.get('#modalSubmit').click();
 
+
+
+    // cy.get('#div239A table thead:first-child').each( (element, index, $list) => {
+    //   // either place
+    // } );
+
+    // cy.get('#239aTable thead:first-child').should('have.text', headingsForEachClassroomReportTable[0]);
+    // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[1]);
+    // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[2]);
+    // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
+    // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
+
+    //check table header elements
+    for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
+      cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+    }
+
+  });
+
+
+  it ('testTableElementsForSpecificRoomNumber', ()=>{
+    // TODO determine if its better to use { [], [], ...} or [ [], [], ... )
+    const expectedContent = [
+      ['2023-4', 'COOS291', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
+    ];
+
+
+  });
+
+  it('testCheckingRoomNumberElementForAllConflictedRooms',()=>{
+
+    //go directly to classroomConflictReport form page and submit the classroom info on the form
+    cy.visit('localhost:3000/classroomConflictReport');
+    cy.get('#allclassroomSelect').check();
+    cy.get('#modalSubmit').click();
+
+    //check room number element
+    cy.get('h2').contains('Room Number: 239A');
+    cy.get('h2').contains('Room Number: 239B');
+
+  });
+
+
+
+
+  it ('testTableHeaderElementsForAllConflictedRooms', ()=>{
+    //go directly to classroomConflictReport form page and submit the classroom info on the form
+    cy.visit('localhost:3000/classroomConflictReport');
+    cy.get('#allclassroomSelect').check();
+    cy.get('#modalSubmit').click();
+
+    //check table header elements
+    for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
+      cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+      cy.get(`#239bTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+    }
+
+  });
+
+
+  it ('testTableElementsForSpecificRoomNumber', ()=>{
+    // TODO determine if its better to use { [], [], ...} or [ [], [], ... )
+    const expectedContent = [
+      ['2023-4', 'COOS291', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
+    ];
+
+
+  });
 
 });
