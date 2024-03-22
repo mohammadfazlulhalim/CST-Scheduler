f5a85d5ac9070680da4d73927221ecb6a793f5dd
Mohammad <halim5471@saskpolytech.ca>
2024-03-22 16:35:04 -0600

    update the classroom conflict report UI tests and ensure that they reflect the comment received on the precode demo

--- __tests__/functional/ClassroomConflict.func.js
+++ __tests__/functional/ClassroomConflict.func.js
@@ -13,18 +13,7 @@ const ClassroomConflictReportController= require('../../routes/classroomConflict
 const setupTables = require('../../fixtures/ClearAndDefineTables');
 const {instructor1} = require('../../fixtures/Instructor.fix');
 const {program1} = require('../../fixtures/Program.fix');
-// const expect = require("expect");
 
-
-/*
-* VALIDATION ERROR THROWN:- Functional testing - while running Timeslot.create( {
-      {startDate: '2023-05-01', endDate: '2023-05-24', startTime: '8:00', endTime: '9:00', day: 3, group: 'B'};
-}
-
-switching startTime and endTime to 08:00, 09:00 respectively resolves this...
-THOUGH, if we run loaddb - it doesn't complain about the same validation that stops our test.
-*
-* */
 
 describe('Classroom Conflict Report Router', ()=>{
   jest.setTimeout(10000);
@@ -115,64 +104,16 @@ describe('Classroom Conflict Report Router', ()=>{
         {id: 8, startDate: '2023-05-01', endDate: '2023-05-24', startTime: '08:00', endTime: '09:00', day: 3, group: 'A'};
 
 
-    // below timeslots should have same information including the same classroom ID
-    const createdTimeslot1 = await TimeSlot.create(timeslotObj1);
-    await createdTimeslot1.setClassroom(createdClassroom);
-    await createdTimeslot1.setTerm(createdTerm);
-    await createdTimeslot1.setInstructor(createdInstructor);
-    await createdTimeslot1.setProgram(createdProgram);
-    await createdTimeslot1.setCourseOffering(createdCourseOffering);
+    //We setup TermId, courseOfferingId,InstructorId, ProgramId, classroomId  in the time slot object using createNewTimeslotWithAssociations helper function
+    const createdTimeslot1 =  await createNewTimeslotWithAssociations(timeslotObj1,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering );
+    const createdTimeslot2 = await  createNewTimeslotWithAssociations(timeslotObj2,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering2 );
+    const createdTimeslot3 = await  createNewTimeslotWithAssociations(timeslotObj3,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering );
+    const createdTimeslot4 =await  createNewTimeslotWithAssociations(timeslotObj4,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering );
+    const createdTimeslot5 = await  createNewTimeslotWithAssociations(timeslotObj5,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering2 );
+    const createdTimeslot6 = await  createNewTimeslotWithAssociations(timeslotObj6,createdClassroom, createdTerm,createdInstructor,createdProgram,createdCourseOffering2 );
+    const createdTimeslot7 =await  createNewTimeslotWithAssociations(timeslotObj7,createdClassroom2, createdTerm,createdInstructor,createdProgram,createdCourseOffering2 );
+    const createdTimeslot8 = await  createNewTimeslotWithAssociations(timeslotObj8,createdClassroom2, createdTerm,createdInstructor,createdProgram,createdCourseOffering2 );
 
-    const createdTimeslot2 = await TimeSlot.create(timeslotObj2);
-    await createdTimeslot2.setClassroom(createdClassroom);
-    await createdTimeslot2.setTerm(createdTerm);
-    await createdTimeslot2.setInstructor(createdInstructor);
-    await createdTimeslot2.setProgram(createdProgram);
-    await createdTimeslot2.setCourseOffering(createdCourseOffering2);
-
-    const createdTimeslot3 = await TimeSlot.create(timeslotObj3);
-    await createdTimeslot3.setClassroom(createdClassroom);
-    await createdTimeslot3.setTerm(createdTerm);
-    await createdTimeslot3.setInstructor(createdInstructor);
-    await createdTimeslot3.setProgram(createdProgram);
-    await createdTimeslot3.setCourseOffering(createdCourseOffering);
-
-    const createdTimeslot4 = await TimeSlot.create(timeslotObj4);
-    await createdTimeslot4.setClassroom(createdClassroom);
-    await createdTimeslot4.setTerm(createdTerm);
-    await createdTimeslot4.setInstructor(createdInstructor);
-    await createdTimeslot4.setProgram(createdProgram);
-    await createdTimeslot4.setCourseOffering(createdCourseOffering);
-
-    const createdTimeslot5 = await TimeSlot.create(timeslotObj5);
-    await createdTimeslot5.setClassroom(createdClassroom);
-    await createdTimeslot5.setTerm(createdTerm);
-    await createdTimeslot5.setInstructor(createdInstructor);
-    await createdTimeslot5.setProgram(createdProgram);
-    await createdTimeslot5.setCourseOffering(createdCourseOffering2);
-
-    const createdTimeslot6 = await TimeSlot.create(timeslotObj6);
-    await createdTimeslot6.setClassroom(createdClassroom);
-    await createdTimeslot6.setTerm(createdTerm);
-    await createdTimeslot6.setInstructor(createdInstructor);
-    await createdTimeslot6.setProgram(createdProgram);
-    await createdTimeslot6.setCourseOffering(createdCourseOffering);
-
-
-    const createdTimeslot7 = await TimeSlot.create(timeslotObj7);
-    await createdTimeslot7.setClassroom(createdClassroom2);
-    await createdTimeslot7.setTerm(createdTerm);
-    await createdTimeslot7.setInstructor(createdInstructor);
-    await createdTimeslot7.setProgram(createdProgram);
-    await createdTimeslot7.setCourseOffering(createdCourseOffering);
-
-
-    const createdTimeslot8 = await TimeSlot.create(timeslotObj8);
-    await createdTimeslot8.setClassroom(createdClassroom2);
-    await createdTimeslot8.setTerm(createdTerm2);
-    await createdTimeslot8.setInstructor(createdInstructor);
-    await createdTimeslot8.setProgram(createdProgram);
-    await createdTimeslot8.setCourseOffering(createdCourseOffering);
   } );
 
   /**
@@ -216,12 +157,7 @@ describe('Classroom Conflict Report Router', ()=>{
     const classroomInstance = await Classroom.findOne({where: {roomNumber: classroomObj.roomNumber}});
     const termInstance1 = await Term.findOne({where: {termNumber: termObj.termNumber}});
 
-
-
-
-
-    // TODO implement parameter change router as well!
-    // sends in the term and classroom to the function in router
+     // sends in the term and classroom to the function in router
     const results = await ClassroomConflictReportController.generateTimeslotsTest(classroomInstance, termInstance1);
     //expected result should be a 2D array of length
     expect(results.length).toBe(2);
--- cypress/e2e/classroomConflictReport.cy.js
+++ cypress/e2e/classroomConflictReport.cy.js
@@ -17,8 +17,18 @@
 * */
 
 describe( 'classroom conflict report page', ()=>{
-  const headingsForEachClassroomReportTable = ['Term', 'Course Code', 'Weekday', 'Start Time', 'End Time', 'Instructor'];
+  const headingsForEachClassroomReportTable = ['Term ', 'Course Code ', 'Weekday ', 'Start Time ', 'End Time ', 'Instructor ','Program '];
   const rowContentsForConflictTimeslotsTable = [
+    [ '1 2023-08-01','COOS293', '1', '08:00', '09:00','Bryce Barrie','Computer Systems Technology'],
+    ['1 2023-08-01','','1','08:30',	'10:30', ' ', '', ''],
+    ['1 2023-08-01', 'SEM283','3','10:00','11:00','Ernesto Basoalto','Computer Systems Technology'],
+    ['1 2023-08-01','COHS280','3',	'11:00',	'12:00','firstName Holtslan', 'Computer Systems Technology'],
+    ['1 2023-08-01', 'CWEB280',	'4',	'10:00',	'11:00',	'Donovan Onishenko', 'Computer Systems Technology'],
+    ['1 2023-08-01', 'COOS291',	'4',	'11:00',	'12:00',	'Wade Lahoda', 'Computer Systems Technology'],
+    ['1 2023-08-01', '',		'5',	'08:00',	'14:00', ' ','' ],
+    ['1 2023-08-01',	'COOS293',	'5',	'09:00',	'10:00',	'Jason Schmidt', 'Computer Systems Technology'],
+    ['1 2023-08-01',	'MATH282',	'5',	'13:00',	'14:00',	'Micheal Grzesina', 'Computer Systems Technology'],
+    ['1 2023-08-01', '',		'5',	'13:00',	'14:00',' ','']
 
   ];
 
@@ -35,22 +45,24 @@ describe( 'classroom conflict report page', ()=>{
     cy.visit('localhost:3000');
     cy.contains('Reports').click();
     cy.get('.nav-item.dropdown .dropdown-menu a[href="/classroomConflictReport"]').click();
-    cy.get('#classroomConflictModal').should('be.visible');
+
     // Assert that the URL has changed to the classroom Conflict Report page
     cy.url().should('include', '/classroomConflictReport');
+    cy.get('#classroomConflictModal').should('be.visible');
+    //Check that term filed has empty value
+    cy.get('#termSelect').should('have.value',null);
+    // Check that classroom field has empty value
+    cy.get('#classroomSelect').should('have.value',null);
+    // Check that Enter button is disabled
+    cy.get('#modalSubmit').should('be.disabled');
   });
 
   it('testPageLoading', () => {
     // go directly to classroomConflictReport form
     cy.visit('localhost:3000/classroomConflictReport');
-    // Check that Enter button is disabled
-    cy.get('#modalSubmit').should('be.disabled');
-    // Check that classroom field can be entered
-    cy.contains('Classroom');
     cy.get('#classroomSelect').select(classroomsVisible[0]);
-    // Check that Enter button is enabled
-    cy.get('#modalSubmit').should('be.disabled');
     cy.get('#termSelect').select(termsVisible[0]);
+    //After selecting classroom and term the Generate Report button is enabled
     cy.get('#modalSubmit').should('be.enabled');
     cy.get('#modalSubmit').click();
 
@@ -60,59 +72,30 @@ describe( 'classroom conflict report page', ()=>{
     // Check header for classroom conflict page
     cy.get('#pageTitleClassroomConflicts').contains('Classroom Conflicts');
 
-    /* testCheckingRoomNumberElementForSpecificRoomNumber */
-
-    // go directly to classroomConflictReport form page and submit the classroom info on the form
-
+    /** testCheckingRoomNumberElementForSpecificRoomNumber */
 
     // check room number element
     cy.get('#selectedClassroom').contains(`Classroom: ${classroomsVisible[0]}`);
-    // Check term
+    // Check term element
     cy.get('#selectedTerm').contains(`Term: ${termsVisible[0]}`);
 
-    /* testTableHeaderElementsForSpecificRoomNumber */
 
-
-    // cy.get('#div239A table thead:first-child').each( (element, index, $list) => {
-    //   // either place
-    // } );
-
-    // cy.get('#239aTable thead:first-child').should('have.text', headingsForEachClassroomReportTable[0]);
-    // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[1]);
-    // cy.get('#239aTable thead:nth-child(2)').should('have.text', headingsForEachClassroomReportTable[2]);
-    // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
-    // cy.get('#239aTable thead:nth-child(2)').should('have.text', 'Course Code');
-
+    /** testTableHeaderElements */
     // check table header elements
     for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
-      cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+     cy.get(`#reportTable tbody th:eq(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
     }
-    //body > div > div > div.p-4 > table > tbody > tr:nth-child(1) > th:nth-child(1)
-
-    // });
-    //
-    //
-    // it('testTableHeaderElementsForAllConflictedRooms', ()=>{
-    //   // go directly to classroomConflictReport form page and submit the classroom info on the form
-    //   cy.visit('localhost:3000/classroomConflictReport');
-    //   cy.get('#allclassroomSelect').check();
-    //   cy.get('#modalSubmit').click();
-
-
-    /* testTableHeaderElementsForAllConflictedRooms */
+    //to select element inside 2D array we will use count which will be incremented inside the for loop
+    let count =0;
+    /** testTableElements */
     // check table header elements
-    for (let i = 0; i < headingsForEachClassroomReportTable.length; i++) {
-      cy.get(`#239aTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
-      cy.get(`#239bTable thead:nth-child(${i})`).should('have.text', headingsForEachClassroomReportTable[i]);
+    for (let i = 0; i < rowContentsForConflictTimeslotsTable.length; i++) {
+      for (let j = 0; j < rowContentsForConflictTimeslotsTable[0].length; j++) {
+
+        cy.get(`#reportTable tbody td:eq(${count++})`).should('have.text', rowContentsForConflictTimeslotsTable[i][j]);
+      }
     }
   });
 
 
-  it('testTableElementsForSpecificRoomNumber', ()=>{
-    // TODO fix the checks here to match with values on the database!! otherwise test will break!
-    const expectedContent = [
-      ['2023-4', 'COOS291', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
-      ['2023-4', 'COSC280', 'Monday', '8:00', '9:00', 'Wade Lahoda'],
-    ];
-  });
 });
