879c84fa2f918541e239ee46888e0ed92771a895
Lysholm <lysholm9452@saskpolytech.ca>
2024-04-11 13:51:21 -0600

    Figured out how to do HBS images, switched bootswatch theme to pulse -Brandon

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('views/images'));
 
 // routers
 app.use('/', indexRouter);
